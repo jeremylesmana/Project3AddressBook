34eb10bef9124b01f63ea81d803c5e4f7dd3b9b5
Andrew Gravino <aggravino@cpp.edu>
2021-10-14 17:26:25 -0700

    Put my stuff up to date before fusing

--- address_book_menu.c
+++ address_book_menu.c
@@ -147,18 +147,33 @@ Status search_contact(AddressBook *address_book)
 
 Status edit_contact(AddressBook *address_book)
 {
-	/* Add the functionality for edit contacts here */
 	/*
 	we want to be able to grab input
 	we'll print out prompts based on the user's inputs that delve into sections
+	we first have to search what mode we want from the main menu and that's 3 or edit contact so keep that in mind
 	*/
 
+	char tempStringInput[32];
+	
+	menu_header("Search contact by: \n"); //update the menu name
+	printf("0. Back\n");
+	printf("1. Name\n");
+	printf("2. Phone No\n");
+	printf("3. Email ID\n");
+	printf("4. Serial No\n");
 	/*
-	we first have to search what mode we want and that's 3 or edit contact so keep that in mind
 
 	we then have to specify how we'll identify the person; it can be name, phone number, email, or a serial number
 	- but there's also a back button to revert to a previous so implement like a while loop 
-
+*/
+	int searchChoice = scanf("%d");
+	if(searchChoice == 0){
+		return e_back;
+	}
+	else if(searchChoice == 1){
+		printf("Enter the Name: ")
+	}
+/*
 	once the option is selected, you'll input the actual value 
 	- (i.e. 1 corresponds to name so the user types in the name of the person in a prompt after)
 
@@ -169,8 +184,12 @@ Status edit_contact(AddressBook *address_book)
 	then we ask for what serial number they are specifically so we can work on that specific contact
 
 	afterward, we can select what aspect of their contact we want to adjust with a number input again
-	- we prompt them twice asking what is the index of the item we want to change
+	- we prompt them twice asking what is the position of the item we want to change
+		- this means that we can have two phone numbers for example if they specify a position value that is + 1 the original amount
+			- Note: I say position because it's actually one more than normal index starting at 0, refer to 5.19
 	- the other prompt asks what we want to adjust the item to
+		- if we specified the point previous to this, pressing enter by itself deletes the new entry
+		- if we put an actual value though, then it gets saved along the new position
 
 	then it shows us an updated version of the contact we adjusted 
 
